be0650ce3588876bb094526a179cac0b00591249
Clifford William <ccliffordwilliam@gmail.com>
2024-07-14 20:29:18 +0700

    feat: first page

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const baseUrl = window.location.origin;
 // Example data for services
 const services = {
     1: { 
-        title: "First Service", 
-        content: "This is the first service.",
+        title: "Bathroom", 
+        content: "Comprehensive Bathroom Cleaning for Ultimate Freshness.",
+        src: "./public/bathroom.jpeg",
+        icon: "./public/bathroom_icon.svg",
         list: ["Feature 1", "Feature 2", "Feature 3"],
         faqs: [
             { title: "Faq 1", content: "content 1" },
@@ -13,8 +15,10 @@ const services = {
         ]
     },
     2: { 
-        title: "Second Service", 
-        content: "This is the second service.",
+        title: "Livingroom", 
+        content: "Impeccable Home Cleaning for Every Corner.",
+        src: "./public/livingroom.jpeg",
+        icon: "./public/livingroom_icon.svg",
         list: ["Feature A", "Feature B", "Feature C"],
         faqs: [
             { title: "Faq a", content: "content a" },
@@ -23,8 +27,70 @@ const services = {
         ]
     },
     3: {
-        title: "Third Service",
-        content: "This is the third service.",
+        title: "Bedroom",
+        content: "Create a Serene and Tidy Bedroom Retreat.",
+        src: "./public/bedroom.jpeg",
+        icon: "./public/bedroom_icon.svg",
+        list: ["Feature X", "Feature Y", "Feature Z"],
+        faqs: [
+            { title: "Faq x", content: "content x" },
+            { title: "Faq y", content: "content y" },
+            { title: "Faq z", content: "content z" }
+        ]
+    },
+    4: {
+        title: "Laundry Room",
+        content: "Keep Your Laundry Room Spotless and Efficient.",
+        src: "./public/laundryroom.png",
+        icon: "./public/laundryroom_icon.svg",
+        list: ["Feature X", "Feature Y", "Feature Z"],
+        faqs: [
+            { title: "Faq x", content: "content x" },
+            { title: "Faq y", content: "content y" },
+            { title: "Faq z", content: "content z" }
+        ]
+    },
+    5: {
+        title: "Kitchen",
+        content: "Revitalize Your Kitchen with Detailed Cleaning Excellence.",
+        src: "./public/kitchen.jpeg",
+        icon: "./public/kitchen_icon.svg",
+        list: ["Feature X", "Feature Y", "Feature Z"],
+        faqs: [
+            { title: "Faq x", content: "content x" },
+            { title: "Faq y", content: "content y" },
+            { title: "Faq z", content: "content z" }
+        ]
+    },
+    6: {
+        title: "Door",
+        content: "Enhance Your Home’s Entrance with Pristine Doors.",
+        src: "./public/door.png",
+        icon: "./public/door_icon.svg",
+        list: ["Feature X", "Feature Y", "Feature Z"],
+        faqs: [
+            { title: "Faq x", content: "content x" },
+            { title: "Faq y", content: "content y" },
+            { title: "Faq z", content: "content z" }
+        ]
+    },
+    7: {
+        title: "All Room",
+        content: "Comprehensive Cleaning for Every Room in Your Home.",
+        src: "./public/allroom.jpeg",
+        icon: "./public/allroom_icon.svg",
+        list: ["Feature X", "Feature Y", "Feature Z"],
+        faqs: [
+            { title: "Faq x", content: "content x" },
+            { title: "Faq y", content: "content y" },
+            { title: "Faq z", content: "content z" }
+        ]
+    },
+    7: {
+        title: "Additional",
+        content: "Transform Every Room with Detailed Cleaning Services.",
+        src: "./public/other.jpeg",
+        icon: "./public/other_icon.svg",
         list: ["Feature X", "Feature Y", "Feature Z"],
         faqs: [
             { title: "Faq x", content: "content x" },
@@ -66,6 +132,24 @@ function generateServiceLinksHTML(limit = Object.keys(services).length) {
     }).join('');
 }
 
+// Function to generate service card links HTML
+function generateServiceCardLinksHTML(limit = Object.keys(services).length) {
+    return Object.keys(services).slice(0, limit).map(id => {
+        return `
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <img src="${services[id].src}" class="card-img-top" alt="${services[id].title}">
+                <div class="card-body" style="position: relative;">
+                    <img style="position: absolute; top: 0; right: 0; padding: 1rem; transform: translateY(-50%);" src="${services[id].icon}" alt="${services[id].title}">
+                    <h5 class="card-title"><a href="${baseUrl}/service/${id}" data-link class="stretched-link text-decoration-none text-reset card-title">${services[id].title}</a></h5>
+                </div>
+            </div>
+        </div>
+        `;
+    }).join('');
+}
+
+
 // Function to generate HTML for a list
 function generateListHTML(items) {
     return `<ul>${items.map(item => `<li>${item}</li>`).join('')}</ul>`;
@@ -81,7 +165,59 @@ const routes = {
     '/': {
         name: "Home",
         page: `
-        <h1>Home Page</h1>${generateServiceLinksHTML(2)}<a href="${baseUrl}/service" data-link>Service</a>${generateTestimonialsHTML()}
+        <div class="hero-container blue-bg">
+            <div class="hero">
+                <div class="hero-img-left"></div>
+                <img class="img-fluid hero-img" src="./public/hero.png" alt="Hero" width="576" height="640">
+            </div>
+            <img class="bubble" src="./public/bubble.svg" alt="Bubble" width="1920" height="270">
+        </div>
+        <section class="px-4 pt-5 my-5 mt-0" style="background:#F7FAFC;">
+            <div class="container">
+                <h1 class="text-center display-4 fw-bold text-body-emphasis">TOP NOTCH SERVICE FOR YOUR PLACE</h1>
+                <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+                    <div class="feature col">
+                        <div class="feature-icon d-inline-flex align-items-center justify-content-center fs-2 mb-3">
+                            <img class="img-fluid hero-img" src="./public/handshake.svg" alt="Handshake" width="120" height="120">
+                        </div>
+                        <h3 class="fs-2 text-body-emphasis">Reliability and Trust</h3>
+                        <p>Dependable cleaning services you can count on for a pristine home, every time.</p>
+                        </div>
+                        <div class="feature col">
+                        <div class="feature-icon d-inline-flex align-items-center justify-content-center fs-2 mb-3">
+                            <img class="img-fluid hero-img" src="./public/broom.svg" alt="Broom" width="120" height="120">
+                        </div>
+                        <h3 class="fs-2 text-body-emphasis">Customized Care</h3>
+                        <p>Tailored cleaning to meet your specific needs, from routine dusting to decluttering and organizing.</p>
+                        </div>
+                        <div class="feature col">
+                        <div class="feature-icon d-inline-flex align-items-center justify-content-center fs-2 mb-3">
+                            <img class="img-fluid hero-img" src="./public/hearthand.svg" alt="HeartHand" width="120" height="120">
+                        </div>
+                        <h3 class="fs-2 text-body-emphasis">Comprehensive Solutions</h3>
+                        <p>Full-range services, including vacant house cleaning, to ensure every space is spotless and ready.</p>
+                    </div>
+                    </div>
+            </div>
+        </section>
+        <section class="px-4 pt-5 my-5 mt-0">
+            <div class="container">
+                <h1 class="text-center display-4 fw-bold text-body-emphasis">Experience a Sparkling Home Every Day</h1>
+                <p class="lead mb-4 text-center">Comprehensive commercial cleaning services for a healthier environment tailored to your schedule and needs.</p>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    ${generateServiceCardLinksHTML(5)}
+                    <div class="col">
+                        <a href="${baseUrl}/service" data-link class="text-reset text-decoration-none text-wrap fw-bold other-button-card">
+                            Other Services
+                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="white" class="bi bi-arrow-right-circle" viewBox="0 0 40 40">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        ${generateTestimonialsHTML()}
         `,
     },
     '/about': {
@@ -220,12 +356,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // Create the nav
     const nav = document.getElementById('nav');
     nav.innerHTML = `
-    <div class="container-fluid">
-        <a class="navbar-brand" href="/" data-link>Navbar</a>
+    <div class="container">
+        <a class="navbar-brand" href="/" data-link>
+            <img src="./public/logo.svg" alt="Landlady" width="242" height="55">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="flex-grow:0; margin-left: auto;">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 ${populateNavbarUlWithLi()}
                 <li class="nav-item dropdown">
@@ -248,3 +386,5 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 navigateTo(`${baseUrl}/`);
+
+document.getElementById('currentYear').innerText = `© ${new Date().getFullYear()}`;
